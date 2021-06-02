607a244da04d74743f1ff6ef64b13063b50d0508
Divya Dharshini <77479678+DIVYADHARSHINI19@users.noreply.github.com>
2021-06-02 15:44:39 +0530

    Using SELENIUM

--- youtube.py
+++ youtube.py
@@ -3,11 +3,14 @@ driver = webdriver.Chrome(executable_path=r"C:\chromedriver\chromedriver.exe")
 url = "https://www.youtube.com/"
 driver.get(url)
 
+# SEARCH BAR
 searchbar = driver.find_element_by_id("search")
 searchbar.send_keys("python beginners")
 
+# SEARCH BUTTON
 search = driver.find_element_by_id("search-icon-legacy")
 search.submit()
 
+# PLAYING VIDEO
 video = driver.find_element_by_class_name("style-scope ytd-video-renderer")
 video.click()
