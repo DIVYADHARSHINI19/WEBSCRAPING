6ee6e1499b6b7afe9f19115f48589491a9d0c66c
Divya Dharshini <77479678+DIVYADHARSHINI19@users.noreply.github.com>
2021-06-02 15:43:16 +0530

    Using SELENIUM and bs4

--- selenwebscrap.py
+++ selenwebscrap.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
+# USING SELENIUM AND BS4
 driver = webdriver.Chrome(executable_path=r"C:\chromedriver\chromedriver.exe")
 url = 'https://www.myntra.com/bags'
 driver.get(url)
 content = driver.page_source
 soup = BeautifulSoup(content,"html.parser")
 
+# TO FETCH DATA
 for text in soup.find_all('div',{'class':'product-price'}):
     print(text.span.text)
